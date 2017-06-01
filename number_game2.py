224114d93aa1172258af06a1fd249b4269583001
Ted <trunyonworks@gmail.com>
2017-06-01 18:13:59 -0400

    Refactored all code into functions and added a play again option

---
+++ number_game2.py
@@ -0,0 +1,58 @@
+import random 
+
+# Limit number of guesses allowed
+
+# Catch when someone submits a non-integer
+
+
+
+
+# generate a random number between 1 and 10
+def secret_num():
+    secret_num = random.randint(1, 10)
+    return secret_num
+
+    
+def get_guess_from_user():
+    guess = int(input("Guess a number between 1 and 10 > "))
+    return guess
+
+# Play again option
+def play_again(): 
+    while True:
+        new_game = input("Would you like to play again? Y/N> ")
+        if new_game == "Y":
+            start = main()
+            start
+        elif new_game == "N":
+            break
+
+
+
+def main():
+
+    secret_number = secret_num()
+
+    while True:
+
+        # get a number guess from the player
+        users_guess = get_guess_from_user()
+
+        # compare guess to secret number
+        if  users_guess == secret_number:
+            print("Congrats you've won! My number was {}.".format(secret_number))
+            break
+        elif users_guess > secret_number:
+            print("Guess lower")
+            continue
+        elif users_guess < secret_number:
+            print("Guess higher")
+            continue
+
+    
+    return main
+
+
+main()
+play_again()
+
